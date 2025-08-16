fd5216ff15305cc5f29830301242aff45f9c3ff9
JorrusCodes <johnphilipsheats@gmail.com>
2025-08-16 16:59:24 -0400

    Add files via upload

---
+++ Log File Transcription.ps1
@@ -0,0 +1,6 @@
+﻿# Log File transcription
+Remove-Item "C:\Temp\PSScript.log" -Force -ErrorAction SilentlyContinue
+Start-Transcript "C:\Temp\PSScript.log"
+Write-Output "Started script transcription..."
+Write-Output "Ending script transcription..."
+Stop-Transcript
