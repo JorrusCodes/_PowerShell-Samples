38182313c372ff2a4c4d32ad55329b897872671b
JorrusCodes <johnphilipsheats@gmail.com>
2025-08-16 17:48:38 -0400

    Update Get random values.ps1

--- Get random values.ps1
+++ Get random values.ps1
@@ -2,6 +2,10 @@
 # NOTE that the Maximum value is not included in possible results
 Get-Random -Minimum 1 -Maximum 101
 
+# EXAMPLE: Shuffle a string of characters
+$deckOrder = '11112222333344440000000000'
+$randOrder = ($deckOrder -split '' | Sort-Object {Get-Random}) -join ''
+
 # EXAMPLE: Build a random password from a string of possible characters
 $possibleChars = "qwertyuiopasdfghjklzxcvbnmQWERYIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()"
 $passwordValue = ''
@@ -9,4 +13,5 @@ $passwordLength = 18
 For ($i = 0; $i -lt $passwordLength; $i++) {
     $passwordValue += $possibleChars[(Get-Random -Minimum 0 -Maximum ($possibleChars.Length + 1))]
 }
+
 $passwordValue
