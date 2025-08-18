422be8b8c8c840c0134f805b57c160c5618a0982
JorrusCodes <johnphilipsheats@gmail.com>
2025-08-18 06:17:40 -0400

    Add files via upload

---
+++ Create a shortcut.ps1
@@ -0,0 +1,9 @@
+﻿# Create a shortcut on the Public Desktop
+$s = (New-Object -ComObject WScript.Shell).CreateShortcut("C:\Users\Public\Desktop\Notepad++.lnk")
+$s.TargetPath = "C:\Program Files\Notepad++\notepad++.exe"
+$s.WorkingDirectory = "C:\Program Files\Notepad++" # "Start In" path
+$s.Save
+
+# Apply a specific icon
+$iconPath = "C:\Program Files\Notepad++\notepad++.exe"
+$s.IconLocation = "$iconPath, 0"
