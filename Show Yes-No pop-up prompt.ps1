9f2144e39373e9bb93d8de6ae3eb6f69f6a6267e
JorrusCodes <johnphilipsheats@gmail.com>
2025-09-07 14:27:23 -0400

    Add files via upload

---
+++ Show Yes-No pop-up prompt.ps1
@@ -0,0 +1,11 @@
+﻿# Display a Yes/No prompt
+Add-Type -AssemblyName PresentationFramework
+$msgBoxInput = [System.Windows.MessageBox]::Show(
+    "Ok to continue with script?", # MsgBox content text
+    "Confirm", # Pop-up window title
+    "YesNo" # Prompts
+)
+If ($msgBoxInput -eq "No") { Exit }
+If ($msgBoxInput -eq "Yes") {
+    Write-Output "You chose Yes"
+}
